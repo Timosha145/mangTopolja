581d1af7792ef45cc5c407919f65d7cff5c29122
opilane <opilane@HP-CZC6297B9B>
2022-09-13 11:45:59 +0300

    Add project files.

---
+++ mangTopolja/Ese.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangTopolja
+{
+    //    1.    Liides Üksus sisaldab
+    interface IUksus
+    {
+        //1. int-tüüpi parameetriteta meetodit punktideArv;
+        int punktideArv();
+
+        //2. String-tüüpi parameetriteta meetodit info.
+        string meetodInfo();
+    }
+
+    //    2. Klass Ese realiseerib liidese Üksus.
+    class Ese : IUksus
+    {
+        //1. Klassis on privaatsed isendiväljad järgmise info jaoks: nimetus(String) ja punktide arv(int).
+        private string nimetus;
+        private int punktideArv;
+
+        //2. Klassis peab olema kahe parameetriga konstruktor, mille abil saab määrata nimetuse ja punktide
+        Ese(string nimetus, int punktideArv)
+        {
+            this.nimetus = nimetus;
+            this.punktideArv = punktideArv;
+        }
+
+        //3. Meetod punktideArv tagastab punktide arvu.
+        int IUksus.punktideArv()
+        {
+            return punktideArv;
+        }
+
+        //4. Meetod info tagastab selle eseme nimetuse.
+        public string meetodInfo()
+        {
+            return nimetus;
+        }
+
+    }
+
+
+    //List<Ese> eseList = new List<Ese>();
+
+    //StreamReader sr = new StreamReader(@"..\..\..\info.txt");
+    //string text;
+    //        while ((text = sr.ReadLine()) != null)
+    //        {
+    //            string[] rida = text.Split(';');
+    //            foreach (var item in rida)
+    //            {
+    //                eseList.Add(new Ese(rida[0], int.Parse(rida[1])));
+    //            }             
+    //        }
+    //        sr.Close();
+}
---
+++ mangTopolja/Tegelene.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangTopolja 
+{
+    //    3. Klass Tegelane realiseerib liidese Üksus.
+    class Tegelene : IUksus
+    {
+        //1. Klassis on privaatsed isendiväljad järgmise info jaoks: nimi(String) ja esemete nimekiri
+        private string nimi;
+        List<Ese> eseList = new List<Ese>();
+
+        //2. Klassis peab olema ühe parameetriga konstruktor, mille abil saab määrata nime.
+        Tegelene(string nimi)
+        {
+            this.nimi = nimi;
+        }
+
+        //3. Äsjaloodud tegelasel ei ole ühtegi eset. Eseme lisamiseks peab olema meetod lisaEse, mis jätab argumendiks antud eseme meelde.
+        public void lisaEse()
+        {
+            Tegelene tegelene = new Tegelene(nimi);
+        }
+
+        //3. Meetod punktideArv tagastab punktide arvu.
+        int IUksus.punktideArv()
+        {
+            return punktideArv;
+        }
+
+        //4. Meetod info tagastab selle eseme nimetuse.
+        public string meetodInfo()
+        {
+            return nimetus;
+        }
+    }
+}
+
+    //        3. Klass Tegelane realiseerib liidese Üksus.
+    //1. Klassis on privaatsed isendiväljad järgmise info jaoks: nimi(String) ja esemete nimekiri
+    //(List<Ese>).
+    //2. Klassis peab olema ühe parameetriga konstruktor, mille abil saab määrata nime.
+    //3. Äsjaloodud tegelasel ei ole ühtegi eset. Eseme lisamiseks peab olema meetod lisaEse, mis jätab
+    //argumendiks antud eseme meelde.
+    //4. Meetod punktideArv tagastab tegelase esemete punktide arvude summa.
+    //5. Meetod info tagastab tegelase info tekstina, näidates tegelase nime, esemete arvu ja punktide
+    //arvu.
+    //6. Klassis peab olema ka meetod väljastaEsemed, kus väljastatakse ekraanile tegelase esemed nii,
+    //et iga ese on eraldi real.
+    //7. Klass Tegelane realiseerib liidese Comparable<Tegelane>, kusjuures compareTo meetod
+    //realiseeritakse nii, et võrdlemine toimub esemete arvu alusel. (Näidis kood)
