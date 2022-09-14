51adfe2c4ab73cf1795bda1c78d5a920c227e349
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2022-09-14 23:07:34 +0300

    Update Program.cs

--- mangTopolja/Program.cs
+++ mangTopolja/Program.cs
@@ -1,1 +1,6 @@
-﻿using mangTopolja;
+using mangTopolja;
+using System.Text;
+
+//Andmete fail  esemed.txt salvestage oma arvutisse. Fail peab olema kodeeringus UTF-8.
+Console.OutputEncoding = Encoding.UTF8;
+Peaklass.Mang();
