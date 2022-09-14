21fc28da92a2b4c1eaed351643fcfa9baf4d8bdb
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2022-09-14 23:09:06 +0300

    Add files via upload

---
+++ mangTopolja/IUksus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mangTopolja
+{
+    //    1.    Liides Üksus sisaldab
+    interface IUksus
+    {
+        //1. int-tüüpi parameetriteta meetodit punktideArv;
+        int punktideArv();
+
+        //2. String-tüüpi parameetriteta meetodit info.
+        string meetodInfo();
+    }
+}
